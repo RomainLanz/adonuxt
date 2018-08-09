a13e64f21ae335bfb71819d79ecfbe76b4c85d71
Romain Lanz <romain.lanz@slynova.ch>
2018-08-09 17:20:02 +0200

    feat(template): add default template

--- instructions.js
+++ instructions.js
@@ -6,4 +6,8 @@ module.exports = async function (cli) {
   await cli.makeConfig('nuxt.js', join(__dirname, './config/nuxt.js'))
     .catch((e) => {})
   cli.command.completed('create', 'config/nuxt.js')
+
+  await cli.copy(join(__dirname, 'template'), join(cli.helpers.appRoot(), 'nuxt'))
+    .catch((e) => {})
+  cli.command.completed('create', 'nuxt/')
 }
---
+++ template/pages/index.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>Welcome to Adonuxt!</h1>
+</template>
